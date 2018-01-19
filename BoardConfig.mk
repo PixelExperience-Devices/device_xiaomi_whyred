5d8eb6a7bb4a984e73fa013a1bc155156ce4dc88
Pranav Vashi <neobuddy89@gmail.com>
2018-01-20 00:27:08 +0530

    whyred: Compile libhwui in perf mode
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I0eabad7268ab96ae2e3612c3233047f9f9b39597

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@
 
 DEVICE_PATH := device/xiaomi/whyred
 
+# Compile libhwui in performance mode
+HWUI_COMPILE_FOR_PERF := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
