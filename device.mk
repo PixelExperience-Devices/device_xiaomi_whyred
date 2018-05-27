b66ffd0159fb711638feb8ac64be7ee4f5eb3179
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 18:03:05 +0300

    whyred: Build thermal HAL
    Change-Id: I68be509da28339a4f9465b74ebbaff1f82687aa2

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Thermal
+PRODUCT_PACKAGES += \
+    thermal.sdm660
+
 # Treble makefile
 $(call inherit-product, device/xiaomi/whyred/treble.mk)
 
