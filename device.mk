dec2c6e7e69bee2ba6242fa36ed5edea28c84550
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-07-06 11:38:11 -0300

    whyred: Cleanup and enable some overlays

--- device.mk
+++ device.mk
@@ -171,8 +171,7 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # OMX
 PRODUCT_PACKAGES += \
