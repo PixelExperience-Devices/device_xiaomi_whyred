eb24266b3e5d7949d7934cdb803320a13a3d2a53
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-07-07 17:24:00 -0300

    whyred: Cleanup tree
    * Blame my ocd
    * Remove whitespaces
    * a-z organization
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- lineage.mk
+++ lineage.mk
@@ -24,7 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
@@ -42,4 +41,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=whyred PRODUCT_NAME=whyred
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+
 BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
