3dfb9c3f604e3f1431f18a78352dfe86f9ee90b6
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-07-06 22:34:26 -0300

    whyred: Track and include MiuiCamera
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- device.mk
+++ device.mk
@@ -146,8 +146,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
-    Snap
+    libxml2
 
 # CNE
 PRODUCT_PACKAGES += \
