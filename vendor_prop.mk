d2161cdd568a437b7cdfca23eb9e20b591275e7a
LuK1337 <priv.luk@gmail.com>
2019-02-02 19:34:23 +0100

    whyred: Add debug IMS props to workaround CarrierConfig restrictions
    We support IMS features on carriers not listed in the CarrierConfig,
    so let's use the props to enable it everywhere.
    
    Change-Id: Idd419e3102ad7c1eca81e0d5ff1a2707c50524c7

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -66,6 +66,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.hw.binder.size_kbyte=1024
 
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.mk.maintainer=guaiyihu
