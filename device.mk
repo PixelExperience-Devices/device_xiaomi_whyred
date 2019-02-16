dc469eb814f64de25379e2977b5caaaed4b54270
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-16 15:23:22 +0530

    whyred: copy codec to media_codecs_vendor instead of media_codecs
    Required after https://github.com/AOSPA/android_frameworks_av/commit/496c253766f6550585ce2878824a5bb9a9af0486

--- device.mk
+++ device.mk
@@ -288,7 +288,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
