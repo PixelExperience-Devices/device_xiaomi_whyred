a9aa01be30e50f93b72d3eab17a6809de03ec660
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 00:02:53 -0300

    whyred: Initial changes for caf

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -28,7 +28,7 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 PRODUCT_NAME := aosp_whyred
 PRODUCT_BRAND := Xiaomi
--- device.mk
+++ device.mk
@@ -388,13 +388,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
