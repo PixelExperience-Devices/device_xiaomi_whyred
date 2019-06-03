3a3aa20ce0341aeba1959e92968e3d1e6edf3bda
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-06-03 11:04:42 -0300

    whyred: Remove MiuiCamera
    Revert "whyred: media_profiles: Remove highspeed profiles"
    
    This reverts commit 695e191fa076736925e6e7c5752f723965b41579.
    
    Revert "whyred: Track and include MiuiCamera"
    
    This reverts commit 3dfb9c3f604e3f1431f18a78352dfe86f9ee90b6.

--- device.mk
+++ device.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    libxml2
+    libxml2 \
+    Snap
 
 # CNE
 PRODUCT_PACKAGES += \
