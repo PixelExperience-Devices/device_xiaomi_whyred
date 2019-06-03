8c418ff3adbdc701679b0b84d2f4a25ccca63c9a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-06-03 11:06:43 -0300

    whyred: Track and include GoogleCamera

--- device.mk
+++ device.mk
@@ -144,8 +144,9 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
-    Snap
+    libxml2
+
+$(call inherit-product, packages/apps/GoogleCameraMod/whyred/config.mk)
 
 # CNE
 PRODUCT_PACKAGES += \
