20e82e30ee46949fce9a68437a48934a4fe0b41a
srfarias <erikofarias@gmail.com>
2019-07-25 15:47:56 -0300

    whyred: BoardConfig: remove not needed dex-preopt flags
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -264,15 +264,5 @@ WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
-# Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
-
 # inherit from the proprietary version
 -include vendor/xiaomi/whyred/BoardConfigVendor.mk
