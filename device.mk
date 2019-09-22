5d60e0e887f664fe6683c5fa7180e417672df953
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    whyred: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- device.mk
+++ device.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
