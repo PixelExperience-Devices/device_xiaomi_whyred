ca30671c2f54a0032651b8d1457fc07c514dbd5d
Baalaji <baalajimaestro@raphielgang.org>
2019-11-04 16:12:46 +0530

    [TEMP]: Ignore Neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -236,6 +236,7 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Telephony
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
