bfa0d6b496397fd44f51271f0d1cae527c5ff4b8
Eric Farias <erikofarias@gmail.com>
2019-11-16 00:09:59 -0300

    whyred: build system thermal hidl 1.1
    Signed-off-by: Eric Farias <erikofarias@gmail.com>

--- device.mk
+++ device.mk
@@ -431,6 +431,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.1 \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
