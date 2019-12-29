fba1879514ae1488b2da7f13ef2936d5cf1093be
srfarias <erikofarias@gmail.com>
2019-12-29 05:06:20 +0000

    whyred: call dalvik head config
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- device.mk
+++ device.mk
@@ -162,6 +162,9 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk )
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
