b5d2bfa48c18ac60271738b7484cfafd7b4ba439
Jonas Cardoso <jonascard60@gmail.com>
2020-01-08 13:20:01 -0300

    whyred: releasetools: Fix on incremental updates

--- releasetools.py
+++ releasetools.py
@@ -18,16 +18,18 @@ import common
 import re
 
 def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
+  input_zip = info.input_zip
+  AddModemAssertion(info, input_zip)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
+  input_zip = info.target_zip
+  AddModemAssertion(info, input_zip)
   return
 
-def AddTrustZoneAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(.+)$', android_info)
+def AddModemAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   if m:
     tz_version, build_version = m.group(1).split('|')
     if tz_version and '*' not in tz_version:
@@ -36,3 +38,4 @@ def AddTrustZoneAssertion(info, input_zip):
                    'with this build."););'.format(tz_version, build_version))
       info.script.AppendExtra(cmd)
   return
+
