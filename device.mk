d46aea498da2b83307d0e8b97f961979df09db6a
Raj Jadeja <rajsjadeja4@gmail.com>
2020-06-04 14:46:06 +0530

    whyred: Add authsecret interface
    Change-Id: I4cc9d9dda72510fddff7a9fc6c256689be9f1c96

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Inherit properties.mk
 $(call inherit-product, $(DEVICE_PATH)/properties.mk)
 
+# AuthSecret 
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
