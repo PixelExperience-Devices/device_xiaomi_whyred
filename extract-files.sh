ebcac1d416bf70d2fa6c5f327a95245f10cc7bbd
SakilMondal <mondalsakil@gmail.com>
2020-10-14 14:57:18 +0000

    whyred: PixelExperience bringup (1/2)
    Change-Id: I43e3e5182a6bf15b26da41e9f8cb185d062b8985
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -27,8 +27,8 @@ export DEVICE_COMMON=sdm660-common
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+AOSP_ROOT="$MY_DIR"/../../..
+DEVICE_BLOB_ROOT="$AOSP_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
 patchelf --add-needed libcamera_sdm660_shim.so "$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
 
