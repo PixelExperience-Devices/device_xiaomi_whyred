78dae05efe6d94891241c5f02183f7debe1e0097
SakilMondal <mondalsakil@gmail.com>
2020-12-15 21:56:11 +0530

    whyred: Update Build Fingerprint from Redfin RQ1A.201205.010
    Change-Id: I2bb13f8dd94def5721077dfb21c45e008e426a30
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -44,6 +44,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.201205.010 6953398 release-keys"
 
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
