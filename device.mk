dbc55eadc1d91dea24b384c60fb707e1e8d7abfb
SakilMondal <mondalsakil@gmail.com>
2021-01-04 12:36:44 +0530

    whyred: Add EAS_POWERHINT_VARIANT
    Change-Id: I515ecd7bdfe2f69006ac09644f016b8f215c2112

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage
 
+# Powerhint
+EAS_POWERHINT_VARIANT := sdm636
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
