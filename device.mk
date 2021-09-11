b07df9b2e71bb6c440e78d1b73453114d048d6c6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-11 18:23:20 +0200

    whyred: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: If00badd6dc12e0245c74bba12e4816361091e754

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 
 DEVICE_PATH := device/xiaomi/whyred
 
+# PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
+PRODUCT_SHIPPING_API_LEVEL := 27
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
