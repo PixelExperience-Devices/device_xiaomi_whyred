e498aaf1154b5cdb67abf692d6daa5fc74edcb37
SakilMondal <mondalsakil@gmail.com>
2021-10-06 20:14:13 +0000

    whyred: Build vendor variants of hidl HALs
    Change-Id: Iad29e8c6634c8689f83b3fecde4282eaeaaf116b

--- device.mk
+++ device.mk
@@ -29,11 +29,13 @@ BOARD_HAVE_QCOM_FM := true
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0.vendor \
     android.hardware.gatekeeper@1.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0.vendor \
     android.hardware.keymaster@3.0-service
 
 # Overlays
