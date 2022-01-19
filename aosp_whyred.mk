934bcc8c54f96476663686f0c2a90b295a5d89f8
jhenrique09 <jhsv09@gmail.com>
2022-01-19 03:41:17 +0000

    whyred: Enable quick tap
    Change-Id: I86135ea3c4fb9063f5158f4f7ddd2d7e0f6b8062

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -15,6 +15,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
