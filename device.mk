d326652c9b3cf6dd470bd2c3389284ab83538184
SakilMondal <mondalsakil@gmail.com>
2022-07-18 06:04:06 +0000

    whyred: Decommonize Powerhint
    Change-Id: If89761a859b848e5ba0ab04e8101b097630b7366

--- device.mk
+++ device.mk
@@ -44,7 +44,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay-pe
 
 # Powerhint
-EAS_POWERHINT_VARIANT := sdm636
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Ramdisk
 PRODUCT_PACKAGES += \
